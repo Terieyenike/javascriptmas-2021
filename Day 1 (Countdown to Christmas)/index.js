c7411c45b2acfe51c4a66a5d4f0bd64751018cef
Teri Eyenike <teyenike1@gmail.com>
2021-12-02 18:57:56 +0100

    Add: structure of the countdown

---
+++ Day 1 (Countdown to Christmas)/index.js
@@ -0,0 +1,34 @@
+const countdownDisplay = document.getElementById('countdown-display');
+const countdownText = document.querySelector('.countdown-text');
+const time = document.querySelector('.time');
+
+const christmasDay = new Date('12/25/2021');
+
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+let timerId;
+
+function renderCountdown() {
+  const christmas = 25;
+
+  const today = String(new Date().getDate()).padStart(2, '0');
+  let remainingTime = christmas - today;
+
+  if (remainingTime <= 0) {
+    countdownText.textContent = 'Merry Christmas Scrimmers';
+    clearInterval();
+    return;
+  }
+
+  countdownDisplay.textContent = remainingTime;
+
+  const countdownToday = christmasDay - new Date();
+  const hours = Math.floor((countdownToday % day) / hour);
+  const minutes = Math.floor((countdownToday % hour) / minute);
+  const seconds = Math.floor((countdownToday % minute) / second);
+  time.innerHTML = `${hours} hrs ${minutes} mins ${seconds} secs`;
+}
+
+timerId = setInterval(renderCountdown, second);
