a8abf3bbd54183596b14aff510488524ecacd2f5
Teri Eyenike <teyenike1@gmail.com>
2021-12-04 21:20:48 +0100

    Add: functionality of the christmas shopping checklist

---
+++ Day 4 (Shopping checklist)/app.js
@@ -0,0 +1,80 @@
+const shoppingForm = document.querySelector('.shopping');
+const list = document.querySelector('.list');
+
+let items = [];
+
+function mirrorToLocalStorage() {
+  localStorage.setItem('items', JSON.stringify(items));
+}
+
+function restoreFromLocalStorage() {
+  const IsItems = JSON.parse(localStorage.getItem('items'));
+
+  if (IsItems.length) {
+    items.push(...IsItems);
+    list.dispatchEvent(new CustomEvent('itemsUpdated'));
+  }
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const name = e.currentTarget.item.value;
+
+  if (!name) return;
+
+  const item = {
+    name,
+    id: Date.now(),
+    complete: false,
+  };
+  items.push(item);
+  e.target.reset();
+  list.dispatchEvent(new CustomEvent('itemsUpdated'));
+}
+
+function displayItems() {
+  const html = items
+    .map(
+      (item) => `<li class="shopping-item">
+      <input type="checkbox" value="${item.id}" ${item.complete && 'checked'}>
+      <span class="itemName">${item.name}</span>
+      <button aria-label="Remove ${item.name}" value="${
+        item.id
+      }">&times;</button>
+      </li>`
+    )
+    .join('');
+  list.innerHTML = html;
+}
+
+function deleteItem(id) {
+  items = items.filter((item) => item.id !== id);
+  list.dispatchEvent(new CustomEvent('itemsUpdated'));
+}
+
+function markAsComplete(id) {
+  const itemRef = items.find((item) => item.id === id);
+  itemRef.complete = !itemRef.complete;
+
+  list.dispatchEvent(new CustomEvent('itemsUpdated'));
+}
+
+list.addEventListener('itemsUpdated', displayItems);
+
+list.addEventListener('itemsUpdated', mirrorToLocalStorage);
+
+shoppingForm.addEventListener('submit', handleSubmit);
+
+list.addEventListener('click', (e) => {
+  const id = parseInt(e.target.value);
+
+  if (e.target.matches('button')) {
+    console.log(deleteItem(id));
+  }
+
+  if (e.target.matches('input[type="checkbox"]')) {
+    markAsComplete(id);
+  }
+});
+
+restoreFromLocalStorage();
