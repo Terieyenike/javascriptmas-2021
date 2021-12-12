a6990cda4a55c44076088b6c552a7e4f55e1088b
Teri Eyenike <teyenike1@gmail.com>
2021-12-12 23:01:14 +0100

    Fix: links to the slider image

--- Day 11 (Christmas carousel)/app.js
+++ Day 11 (Christmas carousel)/app.js
@@ -3,15 +3,15 @@ const next = document.getElementById('next');
 const img = document.getElementById('img');
 const imgs = [
   {
-    src: './images/ojet.jpg',
+    src: 'https://user-images.githubusercontent.com/25850598/145730916-64190a36-0072-44ea-91d5-b51b36a3e000.jpg',
     alt: 'christmas village at night with snow and christmas tree',
   },
   {
-    src: './images/teri.jpeg',
+    src: 'https://user-images.githubusercontent.com/25850598/145730922-34804cd4-8805-4f6f-b16a-f03bfcb5985e.jpeg',
     alt: 'white and gold wrapped present on white table with snowflake decorations',
   },
   {
-    src: './images/terieyenike.jpeg',
+    src: 'https://user-images.githubusercontent.com/25850598/145730924-6acf1ec0-0ba3-4190-ad7b-e70b24d12fc5.jpeg',
     alt: 'small black dog and small beige dog looking out the window at snow next to Christmas wreath',
   },
 ];
